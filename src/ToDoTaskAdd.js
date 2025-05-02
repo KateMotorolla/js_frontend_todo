5b057518652953df48d5059e352b5ac490692ad4
Kate <keys-keys-03@mail.ru>
2025-05-02 22:48:36 +0300

    finish

--- src/ToDoTaskAdd.js
+++ src/ToDoTaskAdd.js
@@ -55,18 +55,28 @@ class ToDoTaskAddInner extends React.Component {
 
   render() {
     return (
-      <div className = "Add">
-          < NavLink to = '/'>Back to list</NavLink>
+      <div className="card-hover-shadow-2x mb-3 card">
+        <div className="card-header-tab card-header">
+          <div className="card-header-title font-size-lg text-capitalize font-weight-normal">
+              <i className="fa fa-tasks"></i>&nbsp;Add Task
+          </div>  
+        </div>
           <form onSubmit={this.onAddFormSubmit} >
-            <input type="text" value = {this.state.name} onChange = {this.onNameChange} placeholder = "Name"/> 
-            <input type="text" value = {this.state.description} onChange = {this.onDescriptionChange} placeholder = "Description"/>
-            <input type="submit" value = "Add" />
+            <div className='widget-content'>
+              <div className='widget-content-wrapper'>
+                <input type="text" value={this.state.name} onChange={this.onNameChange} placeholder='name' className='form-control'/>
+                <input type="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder='Description' className='form-control'/>
+                <input type="submit" value="Add" className='btn btn-primary'/>
+              </div>
+            </div>            
           </form>
+          <div className="d-block text-right card-footer">
+            <NavLink to='/' className="btn btn-primary">Back to list</NavLink>
+          </div>
       </div>
     )
   }
 }
-
 const ToDoTaskAdd = (props) => {
   return (
     <ToDoTaskAddInner {...props} history = {useNavigate()} />
